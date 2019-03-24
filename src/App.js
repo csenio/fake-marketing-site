8c9d704163cb1e6da20c6bf0b279666a1b965343
Jesco Wuester <jescowuester@gmail.com>
2019-03-24 13:15:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+import logo from "./public/logo.png";
+import instagram from "./public/instagram-brands.svg";
+import twitter from "./public/twitter-brands.svg";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <section className="App">
+          <img className="logo" src={logo} alt="logo" />
+          <div className="glitch__container">
+            <h1 className="glitch">CREATE THE FUTURE</h1>
+            <div className="mask-container">
+              <h1 className="glitch-mask">CREATE THE FUTURE</h1>
+            </div>
+          </div>
+          {/* <div className="glitch__container">
+            <h1 className="glitch small">CREATE THE FUTURE</h1>
+            <div className="mask-container">
+              <h1 className="glitch-mask">CREATE THE FUTURE</h1>
+            </div>
+          </div> */}
+        </section>
+        <section className="App section2">
+          <div className="glitch2__container">
+            <h1 className="glitch2">3.11.2020</h1>
+            <div className="mask-container2">
+              <h1 className="glitch-mask2">3.11.2020</h1>
+            </div>
+          </div>
+          <p className="paragraph">
+            We’re building on the foundations of our brand to make something new
+            and exciting. A way for people to connect and together create the
+            future.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="btn__container">
+            <button>Be a part of the movement</button>
+          </div>
+          <div className="socials">
+            <a target="blank" href="https://www.instagram.com/adidas/?hl=en">
+              <img src={instagram} alt="instagram" />
+            </a>
+            <a target="blank" href="https://twitter.com/adidas">
+              <img src={twitter} alt="twitter" />
+            </a>
+          </div>
+        </section>
+        <section className="section3">
+          <p>
+            this project was created by students as an excercise in viral
+            marketing.
+          </p>
+          <p>
+            This project is not endorsed by Adidas and doesn't reflect the views
+            or opinions of Adidas.
+          </p>
+          <p>
+            created with ❤ by{" "}
+            <a target="blank" href="https://github.com/jescowuester/">
+              @JSco
+            </a>
+          </p>
+        </section>
       </div>
     );
   }
